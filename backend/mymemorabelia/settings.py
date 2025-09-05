0d691641b0a171ed61025cc6d1bb7844f8369d7e
root <root@Vector.localdomain>
2025-09-04 20:51:29 -0600

    .

--- backend/mymemorabelia/settings.py
+++ backend/mymemorabelia/settings.py
@@ -1,4 +1,7 @@
 import os
+from pathlib import Path
+from dotenv import load_dotenv
+
 """
 Django settings for mymemorabelia project.
 
@@ -15,7 +18,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
